89ca2dd3cd547ffde21b60ba305b8c9ac1ade95d
sciencesakura <sakurasakurascience@gmail.com>
2023-12-23 22:53:13 +0900

    Add single module project template

---
+++ single-module/src/main/java/com/sciencesakura/Primes.java
@@ -0,0 +1,39 @@
+package com.sciencesakura;
+
+/**
+ * A utility class for prime numbers.
+ */
+public final class Primes {
+
+  private static final int[] WHEEL = {4, 2, 4, 2, 4, 6, 2, 6};
+
+  private Primes() {
+  }
+
+  /**
+   * Test if a natural number is prime.
+   *
+   * @param n the natural number to test
+   * @return {@code true} if {@code n} is prime, {@code false} otherwise
+   */
+  public static boolean isPrime(int n) {
+    if (n == 2 || n == 3 || n == 5) {
+      return true;
+    }
+    if (n < 2 || n % 2 == 0 || n % 3 == 0 || n % 5 == 0) {
+      return false;
+    }
+    var i = 7;
+    var j = 0;
+    while (i * i <= n) {
+      if (n % i == 0) {
+        return false;
+      }
+      if (j == WHEEL.length) {
+        j = 0;
+      }
+      i += WHEEL[j++];
+    }
+    return true;
+  }
+}
